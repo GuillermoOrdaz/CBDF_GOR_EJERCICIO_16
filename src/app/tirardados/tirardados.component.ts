dc6c0a813354573de9bd488f82cf88d7edae7bd6
Guillermo <pinguino2223@gmail.com>
2024-12-14 21:33:41 -0600

    v1

---
+++ src/app/tirardados/tirardados.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { DadoComponent } from '../dado/dado.component';
+
+@Component({
+  selector: 'app-tirardados',
+  standalone: true,
+  imports: [DadoComponent],
+  templateUrl: './tirardados.component.html',
+  styleUrl: './tirardados.component.css'
+})
+export class TirardadosComponent {
+  n1:number=0
+  n2:number=0
+  n3:number=0
+  resultado:string="Tira para jugar"
+  contador:number=0
+  
+  tirar(){
+    //1..6 con numeros aleatorios
+    this.n1=Math.floor(Math.random()*5)+1
+    this.n2=Math.floor(Math.random()*5)+1
+    this.n3=Math.floor(Math.random()*5)+1
+     if(this.n1==this.n2 && this.n1==this.n3){
+      this.resultado = "Felicidades. Ganaste!!!"
+      this.contador=0
+     }else{
+      this.contador++
+      this.resultado = "Fallaste. Intentalo denuevo "+this.contador
+     }
+  }
+}
